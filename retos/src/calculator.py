a23874577858ed297a378e675fb4b60a56afc5d1
Ismael Ortiz <ismael14566@hotmail.com>
2025-01-23 00:24:45 +0000

    Reto de la clase 03

---
+++ retos/src/calculator.py
@@ -0,0 +1,19 @@
+"""Reto de la clase 03"""
+def suma(a, b):
+    "Retorma la suma de dos numeros"
+    return a + b
+
+def resta(a, b):
+    """Retorma la resta de dos numeros"""
+    return a - b
+
+def division(a, b):
+    """Retorma la division de dos numeros"""
+    return a / b
+
+def multiplicacion(a, b):
+    """Retorma la multiplicacion de dos numeros"""
+    return a * b
+
+def potencia_num(a, b):
+    return a ** b
---
+++ retos/tests/test_calculator.py
@@ -0,0 +1,20 @@
+"""Reto de la clase 03"""
+from retos.src.calculator import suma, resta, multiplicacion, division, potencia_num
+import unittest
+
+class CalculatorTest(unittest.TestCase):
+
+    def test_suma(self):
+        assert suma(2, 3) == 5
+
+    def test_resta(self):
+        assert resta(10, 5) == 5
+
+    def test_multiplicacion(self):
+        assert multiplicacion(5, 3) == 15
+
+    def test_division(self):
+        assert division(10, 2) == 5
+
+    def test_potencia(self):
+        assert potencia_num(5, 6) == 15625
